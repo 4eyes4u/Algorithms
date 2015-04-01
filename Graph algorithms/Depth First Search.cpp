327d58258b82adf529c1be036d0324a39c931865
Kosta Grujcic <kosta.grujcic@hotmail.com>
2015-04-01 18:46:45 +0200

    DFS

---
+++ Graph algorithms/Depth First Search.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+using namespace std;
+
+const int N=1e5+10;
+
+struct node
+{
+    vector<int> adj;
+} g[N];
+
+bool mark[N];
+
+void dfs (int v)
+{
+    mark[v]=true;
+    for (int i=0;i<g[v].adj.size();i++)
+    {
+        if (!mark[g[v].adj[i]])
+        {
+            dfs (g[v].adj[i]);
+        }
+    }
+}
+
+int main()
+{
+
+	return 0;
+}
