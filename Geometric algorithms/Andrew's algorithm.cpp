5e533d711216fe9b9ccfbc95f483d6dcb1d87812
4eyes4u <kosta.grujcic@hotmail.com>
2017-06-13 18:28:22 +0200

    Fix

--- Geometric algorithms/Andrew's algorithm.cpp
+++ Geometric algorithms/Andrew's algorithm.cpp
@@ -11,28 +11,29 @@ using namespace std;
 typedef pair<int, int> pt;
 
 long long ccw(pt o, pt a, pt b) {
-  return 1ll * (a.x - o.x) * (b.y - o.y) - 1ll * (b.x - o.x) * (a.y - o.y);
+        return 1ll * (a.x - o.x) * (b.y - o.y) - 1ll * (b.x - o.x) * (a.y - o.y);
 }
 
 vector<pt> convexHull(vector<pt> pts) {
-  vector<pt> hull;
-  sort(pts.begin(), pts.end());
+        vector<pt> hull;
+        sort(pts.begin(), pts.end());
 
-  for (int i = 0; i < 2; i++) {
-    auto init_size = pts.size();
+        for (int i = 0; i < 2; i++) {
+                auto init_size = pts.size();
 
-    for (auto xt : pts) {
-      while (hull.size() >= 2 + init_size &&
-             ccw(hull[hull.size() - 2], hull.back(), xt) <= 0)
-        hull.pop_back();
-      hull.push_back(xt);
-    }
+                for (auto xt : pts) {
+                        while (hull.size() >= 2 + init_size && ccw(hull[hull.size() - 2], hull.back(), xt) <= 0)
+                                hull.pop_back();
+                        hull.push_back(xt);
+                }
 
-    hull.pop_back();
-    reverse(pts.begin(), pts.end());
-  }
+                hull.pop_back();
+                reverse(pts.begin(), pts.end());
+        }
 
-  return hull;
+        return hull;
 }
 
-int main() { return 0; }
+int main() {
+        return 0;
+}
