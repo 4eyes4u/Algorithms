432dfc492638a4079902c2a335bf46509dc71610
4eyes4u <kosta.grujcic@hotmail.com>
2017-06-15 00:35:58 +0200

    Added ordered statistic set

---
+++ Data structures/Ordered statistic set.cpp
@@ -0,0 +1,41 @@
+/*
+  Data structure: Ordered statistic set
+  Complexity: O(logn) per operation [where n is number of elements in set]
+
+* * *
+set.order_of_key -- returs order of given key that does not have to be in set.
+set.find_by_order -- returns iterator to the element that is on given position in sorted array of keys.
+*/
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef
+tree<
+  int,
+  null_type,
+  less<int>,
+  rb_tree_tag,
+  tree_order_statistics_node_update>
+myset;
+
+myset skup;
+
+int main()
+{
+  skup.insert(5);
+  skup.insert(1);
+  skup.insert(100);
+  skup.insert(15);
+  skup.insert(20);
+
+  printf ("%d\n", skup.order_of_key(17));
+  printf ("%d\n", skup.order_of_key(15));
+  printf ("%d\n", *skup.find_by_order(0));
+  printf ("%d\n", *skup.find_by_order(3));
+
+  return 0;
+}
