1a490359f62d4448813861ab1110d5adeab05b21
4eyes4u <kosta.grujcic@hotmail.com>
2017-06-22 23:45:51 +0200

    Fixed Fenwick tree

--- Data structures/Fenwick tree [2D].cpp
+++ Data structures/Fenwick tree [2D].cpp
@@ -7,35 +7,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int N=1e3+10;
+class FenwickTree {
+private:
+  int **a, n, m;
 
-int bit[N][N];
-
-void update (int row, int c, int val)
-{
-    for (;row<N;row+=row&-row)
-    {
-        int col=c;
-        for (;col<N;col+=col&-col) bit[row][col]+=val;
-    }
-}
-
-int get (int row, int c)
-{
+  int get (int row, int c) {
     int ret=0;
-    for (;row;row-=row&row)
-    {
-        int col=c;
-        for (;col;col-=col&-col) ret+=bit[row][col];
+    for (;row>0;row-=row&-row) {
+      int col=c;
+      for (;col>0;col-=col&-col) ret+=a[row][col];
     }
-}
 
-int get (int r1, int c1, int r2, int c2)
-{
+    return ret;
+  }
+public:
+  FenwickTree (int sizeN, int sizeM) {
+    n=sizeN;
+    m=sizeM;
+    a=(int **)calloc(n, sizeof(int *));
+    for (int i=0;i<n;i++) a[i]=(int *)calloc(m, sizeof(int));
+  }
+
+  int update (int row, int c, int val) {
+    for (;row<=n;row+=row&-row) {
+      int col=c;
+      for (;col<=m;col+=col&-col) a[row][col]+=val;
+    }
+  }
+
+  int get (int r1, int c1, int r2, int c2) {
     return get(r2, c2)-get(r2, c1-1)-get(r1-1, c2)+get(r1-1, c1-1);
-}
+  }
+};
 
-int main()
-{
-    return 0;
+int main() {
+  FenwickTree *bit=new FenwickTree(100, 100);
+
+  bit->update(10, 10, 1);
+  bit->update(50, 10, 2);
+  bit->update(10, 50, 3);
+
+  printf ("%d\n", bit->get(1, 1, 80, 80));
+  printf ("%d\n", bit->get(10, 10, 20, 20));
+  printf ("%d\n", bit->get(8, 9, 50, 10));
+
+  return 0;
 }
