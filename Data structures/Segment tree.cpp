dc782f73b17523149cf03f85b3fe13c110eeb3c9
4eyes4u <kosta.grujcic@hotmail.com>
2017-06-23 00:01:09 +0200

    Fixed segment tree

--- Data structures/Segment tree.cpp
+++ Data structures/Segment tree.cpp
@@ -8,50 +8,66 @@ Merge operation is maximum.
 */
 
 #include <bits/stdc++.h>
-#define mid (left+right)/2
 using namespace std;
 
-const int N=1e5+10;
+class SegmentTree {
+#define mid (left+right)/2
 
-int st[4*N], n;
+private:
+  int *a, n;
+public:
+  SegmentTree (int size) {
+    n=size;
+    a=(int *)calloc(4*size, sizeof(int));
+  }
 
-void init (int idx, int left, int right, int *a)
-{
-    if (left==right)
-    {
-        st[idx]=a[left];
-        return;
+  void init (int idx, int left, int right, int *b) {
+    if (left==right) {
+      a[idx]=b[left];
+      return;
     }
 
-    init (2*idx, left, mid, a);
-    init (2*idx+1, mid+1, right, a);
-    st[idx]=max(st[2*idx], st[2*idx+1]);
-}
+    init(2*idx, left, mid, b);
+    init(2*idx+1, mid+1, right, b);
+    a[idx]=max(a[2*idx], a[2*idx+1]);
+  }
 
-void update (int idx, int left, int right, int x, int val)
-{
-    if (left==right)
-    {
-        st[idx]=val;
-        return;
+  void update (int idx, int left, int right, int x, int val) {
+    if (left==right) {
+      a[idx]=val;
+      return;
     }
 
     if (x<=mid) update (2*idx, left, mid, x, val);
     else update (2*idx+1, mid+1, right, x, val);
-    st[idx]=max(st[2*idx], st[2*idx+1]);
-}
+    a[idx]=max(a[2*idx], a[2*idx+1]);
+  }
 
-int query (int idx, int left, int right, int l, int r)
-{
-    if (l<=left && right<=r) return st[idx];
+  int query (int idx, int left, int right, int l, int r) {
+    if (l<=left && right<=r) return a[idx];
 
     int ret=0;
     if (l<=mid) ret=max(ret, query(2*idx, left, mid, l, r));
     if (r>mid) ret=max(ret, query(2*idx+1, mid+1, right, l, r));
     return ret;
-}
+  }
 
-int main()
-{
-    return 0;
+  int get_size () {
+    return n;
+  }
+};
+
+int main() {
+  SegmentTree *st=new SegmentTree(1000);
+
+  int n=st->get_size();
+  st->update(1, 1, n, 5, 1000);
+  st->update(1, 1, n, 1, 10);
+  st->update(1, 1, n, 512, 256);
+
+  printf ("%d\n", st->query(1, 1, n, 1, 1000));
+  printf ("%d\n", st->query(1, 1, n, 200, 600));
+  printf ("%d\n", st->query(1, 1, n, 1, 4));
+
+  return 0;
 }
