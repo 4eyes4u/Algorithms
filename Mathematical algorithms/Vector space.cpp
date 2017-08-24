751a2d91eedbe276fc047216142796a2ad41c150
4eyes4u <kosta.grujcic@hotmail.com>
2017-08-24 11:22:59 +0200

    Fix

--- Mathematical algorithms/Vector space.cpp
+++ Mathematical algorithms/Vector space.cpp
@@ -9,47 +9,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-namespace VectorSpace
-{
-const int DIM = 32;
+namespace VectorSpace {
+    const int DIM = 32;
 
-int basis[DIM], d = 0;
+    int basis[DIM], d = 0;
 
-void insert(int x)
-{
-    if (!d)
-        return void(basis[d++] = x);
+    void insert(int x) {
+        if (!d) return void(basis[d++] = x);
 
-    for (int i = 0; i < d; i++)
-        x = min(x, x ^ basis[i]);
+        for (int i = 0; i < d; i++) x = min(x, x ^ basis[i]);
+        
+        if (x) {
+            basis[d++] = x;
+            for (int i = d - 1; i > 0; i--)
+                if (basis[i] > basis[i - 1])
+                    swap(basis[i], basis[i - 1]);
+        }
+    }
 
-    if (x)
-    {
-        basis[d++] = x;
-        for (int i = d - 1; i > 0; i--)
-            if (basis[i] > basis[i - 1])
-                swap(basis[i], basis[i - 1]);
+    int get_min(int x) {
+        for (int i = 0; i < d; i++) x = min(x, x ^ basis[i]);
+        return x;
     }
 }
 
-int get_min(int x)
-{
-    for (int i = 0; i < d; i++)
-        x = min(x, x ^ basis[i]);
-    return x;
-}
-}
-
-int main()
-{
+int main() {
     VectorSpace::insert(4);
     VectorSpace::insert(7);
     VectorSpace::insert(8);
     VectorSpace::insert(12);
     VectorSpace::insert(17);
 
-    for (int i = 0; i < VectorSpace::d; i++)
-        printf("%d\n", VectorSpace::basis[i]);
+    for (int i = 0; i < VectorSpace::d; i++) printf("%d\n", VectorSpace::basis[i]);
     printf("%d\n", VectorSpace::get_min(22));
 
     return 0;
