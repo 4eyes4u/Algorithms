4dcae172e1794dae68b0d383145c44a091eb9fc9
4eyes4u <kosta.grujcic@hotmail.com>
2017-12-03 21:46:22 +0100

    Updated Matrix

--- Mathematical algorithms/Matrix.cpp
+++ Mathematical algorithms/Matrix.cpp
@@ -1,72 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int mod=1e9+7;
-const int DIM=50+5;
+const int mod = 1e9 + 7;
+const int DIM = 50 + 5;
 
-struct Matrix
-{
-  int a[DIM][DIM];
-  int *operator [] (int r)
-  {
-    return a[r];
-  }
+struct Matrix {
+    int a[DIM][DIM];
 
-  Matrix (int x=0)
-  {
-    memset(a, 0, sizeof a);
-    if (x) for (int i=0;i<DIM;i++) a[i][i]=x;
-  }
+    int *operator [] (int r) {
+        return a[r];
+    }
+
+    Matrix (int x = 0) {
+        memset(a, 0, sizeof(a));
+        if (x) for (int i = 0; i < DIM; i++) a[i][i] = x;
+    }
 } const I(1);
 
-Matrix operator + (Matrix A, Matrix B)
-{
-  Matrix C;
-  for (int i=0;i<DIM;i++)
-  {
-    for (int j=0;j<DIM;j++)
-    {
-      C[i][j]=(A[i][j]+B[i][j])%mod;
+Matrix operator + (Matrix A, Matrix B) {
+    Matrix C;
+
+    for (int i = 0; i < DIM; i++)
+        for (int j = 0; j < DIM; j++)
+            C[i][j] = (A[i][j] + B[i][j]) % mod;
+
+    return C;
+}
+
+Matrix operator * (Matrix A, Matrix B) {
+    Matrix C;
+
+    for (int i = 0; i < DIM; i++) {
+        for (int j = 0; j < DIM; j++) {
+            long long entry = 0;
+            
+            for (int k = 0; k < DIM; k++) {
+                entry += (A[i][k] * B[k][j]) % mod;
+                if (entry > mod) entry -= mod;
+            }
+
+            C[i][j] = entry;
+        }
     }
-  }
 
-  return C;
+    return C;
 }
 
-Matrix operator * (Matrix A, Matrix B)
-{
-  Matrix C;
-  for (int i=0;i<DIM;i++)
-  {
-    for (int j=0;j<DIM;j++)
-    {
-      long long sum=0;
-      for (int k=0;k<DIM;k++)
-      {
-        sum+=(A[i][k]*B[k][j])%mod;
-        if (sum>mod) sum-=mod;
-      }
-      C[i][j]=sum%mod;
+Matrix operator ^ (Matrix A, long long n) {
+    Matrix C = I;
+
+    while (n) {
+        if (n & 1) C = C * A;
+        n >>= 1;
+        A = A * A;
     }
-  }
 
-  return C;
+    return C;
 }
 
-Matrix operator ^ (Matrix A, long long n)
-{
-  Matrix C=I;
-  while (n)
-  {
-    if (n&1) C=C*A;
-    n>>=1;
-    A=A*A;
-  }
-
-  return C;
-}
-
-int main()
-{
-  return 0;
+int main() {
+    return 0;
 }
