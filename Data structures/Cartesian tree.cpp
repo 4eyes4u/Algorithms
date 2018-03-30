d6b380c73d9024769acfb956f3175b726e4b0dea
4eyes4u <kosta.grujcic@hotmail.com>
2018-03-30 12:42:53 +0200

    Fix

--- Data structures/Cartesian tree.cpp
+++ Data structures/Cartesian tree.cpp
@@ -3,6 +3,7 @@
   Memory complexity: O(n) [whre n is number of nodes]
 
 * * *
+
 Useful for constructing suffix tree, suffix array, treap...
 */
 
@@ -21,66 +22,65 @@ class CartesianTree {
 private:
   Node *root, *last;
 
-  void add (Node *v, Node *prevParent, Node *prev) {
-    if (!prevParent) {
-      v->l=prev;
-      prev->p=v;
-      root=v;
+  void add(Node *v, Node *prev_parent, Node *prev) {
+    if (!prev_parent) {
+      v -> l = prev;
+      prev ->p = v;
+      root = v;
     }
-    else if (prevParent->val<=v->val) {
-      prevParent->r=v;
-      v->l=prev;
-      v->p=prevParent;
-      prev->p=v;
-    }
-    else add(v, prevParent->p, prevParent);
+    else if (prev_parent -> val <= v -> val) {
+      prev_parent -> r = v;
+      v -> l = prev;
+      v -> p = prev_parent;
+      prev -> p = v;
+    } else add(v, prev_parent -> p, prev_parent);
   }
 
-  void inOrder (Node *v) {
-    if (v->l) inOrder(v->l);
-    printf("%d\n", v->val);
-    if (v->r) inOrder(v->r);
+  void in_order (Node *v) {
+    if (v -> l) in_order(v -> l);
+    printf("%d\n", v -> val);
+    if (v -> r) in_order(v -> r);
   }
+  
 public:
   CartesianTree () {
-    root=nullptr;
-    last=nullptr;
+    root = nullptr;
+    last = nullptr;
   }
 
-  void add (int val, int idx) {
-    Node *v=new Node(val, idx);
+  void add(int val, int idx) {
+    Node *v = new Node(val, idx);
 
     if (!root) root=v;
-    else if (last->val<=v->val) {
-      v->p=last;
-      last->r=v;
-    }
-    else add(v, last->p, last);
+    else if (last -> val <= v->val) {
+      v -> p = last;
+      last -> r = v;
+    } else add(v, last -> p, last);
 
-    last=v;
+    last = v;
   }
 
-  void inOrder() {
-    inOrder(root);
+  void in_order() {
+    in_order(root);
   }
 };
 
 int main() {
-  CartesianTree *tree=new CartesianTree();
+  CartesianTree *tree = new CartesianTree();
 
-  tree->add(9, 1);
-  tree->add(3, 2);
-  tree->add(7, 3);
-  tree->add(1, 4);
-  tree->add(8, 5);
-  tree->add(12, 6);
-  tree->add(10, 7);
-  tree->add(20, 8);
-  tree->add(15, 9);
-  tree->add(18, 10);
-  tree->add(5, 11);
+  tree -> add(9, 1);
+  tree -> add(3, 2);
+  tree -> add(7, 3);
+  tree -> add(1, 4);
+  tree -> add(8, 5);
+  tree -> add(12, 6);
+  tree -> add(10, 7);
+  tree -> add(20, 8);
+  tree -> add(15, 9);
+  tree -> add(18, 10);
+  tree -> add(5, 11);
 
-  tree->inOrder();
+  tree -> in_order();
 
   return 0;
 }
