7eeec82c7d0fbf623b31fab55f6776fa42ea0f4d
4eyes4u <kosta.grujcic@hotmail.com>
2018-03-30 12:35:50 +0200

    Fix

--- Data structures/Bitwise trie.cpp
+++ Data structures/Bitwise trie.cpp
@@ -1,45 +1,43 @@
 /*
 Data structure: Bitwise trie
 Time complexity:
-    Add number O(u) [where u is size of universe U]
+    Add number O(U) [where u is size of universe]
     Get O(u) [where u is size of universe U]
 Memory complexity: O(n*u) [where n is number of added numbers and u is size of universe U]
 
 * * *
-Calculating maximum xor for given number.
+
+Calculating maximum XOR for given number.
 */
 
 #include <bits/stdc++.h>
 using namespace std;
 
-const int N=1e5+10;
-const int u=30;
+const int N = 1e5 + 10;
+const int U = 30;
 
 int trie[N][2], k;
 
-void add (int node, int x, int pw=u)
-{
-    if (pw==-1) return;
+void add (int node, int x, int pw=u) {
+    if (pw == -1) return;
 
-    int b=(bool)(x&(1<<pw));
-    if (!trie[node][b]) trie[node][b]=++k;
-    add (trie[node][b], x, pw-1);
+    int b = (bool)(x & (1 << pw));
+    if (!trie[node][b]) trie[node][b] = ++k;
+    add(trie[node][b], x, pw - 1);
 }
 
-int get (int node, int x, int pw=u)
-{
-    if (pw==-1) return 0;
+int get (int node, int x, int pw = U) {
+    if (pw == -1) return 0;
 
-    int b=(bool)(x&(1<<pw));
-    if (trie[node][b^1]) return (1<<pw)+get(trie[node][b^1], x, pw-1);
-    else if (trie[node][b]) return get(trie[node][b], x, pw-1);
-    else return 0;
+    int b = (bool)(x & (1 << pw));
+    if (trie[node][b ^ 1]) return (1 << pw) + get(trie[node][b ^ 1], x, pw - 1);
+    else if (trie[node][b]) return get(trie[node][b], x, pw - 1);
+    return 0;
 }
 
-int main()
-{
-    add (0, 0);
-    printf ("%d", get (0, 7));
+int main() {
+    add(0, 0);
+    printf("%d\n", get (0, 7));
 
     return 0;
 }
