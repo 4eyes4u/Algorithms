808d02ed0f14f198348e7d072b24d36bbbd375ab
4eyes4u <kosta.grujcic@hotmail.com>
2018-09-13 23:55:25 +0200

    Fixed FF

--- Graph algorithms/Ford-Fulkerson.cpp
+++ Graph algorithms/Ford-Fulkerson.cpp
@@ -1,84 +1,79 @@
 /*
-    Algorithm: Ford-Fulkerson algorithm
-    Complexity: O(E * F) [where F is maximum flow]
+    Cache-friendly implementation of Ford-Fulkerson algorithm
+    Complexities [where E is number of edges]:
+        -Time: O(MAX_FLOW * E)
+        -Space: O(E)
 */
 
 #include <bits/stdc++.h>
 using namespace std;
 
-const int N = 1e2 + 10;
-const int M = 1e4 + 10;
-const int inf = 1e9;
+const int N = 1e3 + 10;
 
 struct Edge {
-    int u, v, cap, flow, next;
+    /*
+        opposite is index of edge that is opposite
+        to the given one in the residual graph
+    */
+    int v, cap, flow, opposite;
+};
 
-    Edge(int u = 0, int v = 0, int cap = 0, int flow = 0, int next = -1) {
-        this -> u = u;
-        this -> v = v;
-        this -> cap = cap;
-        this -> flow = flow;
-        this -> next = next;
-    }
-} edges[M];
+vector<Edge> g[N];
+int src, snk, n, m;
 
-int head[N], cnt_edges;
-bool mark[N];
-
-void add_edge(int u, int v, int cap1, int cap2) {
-    // head[v] saves last edge with endpoint v
-    // edge.next saves index of previous head[v]
-    // we can iterate through neighbours by taking edge.next
-
-    edges[cnt_edges++] = *new Edge(u, v, cap1, 0, head[u]);
-    head[u] = cnt_edges - 1;
-
-    edges[cnt_edges++] = *new Edge(v, u, cap2, 0, head[v]);
-    head[v] = cnt_edges-1;
+void add_edge(int u, int v, int cap1, int cap2 = 0) {
+    int opposite_u = g[v].size();
+    int opposite_v = g[u].size();
+    g[u].push_back({v, cap1, 0, opposite_u});
+    g[v].push_back({u, cap2, 0, opposite_v});
 }
 
-int dfs(int v, int flow, int snk) {
-    mark[v] = 1;
-    if (v == snk) return flow;
+namespace FordFulkerson {
+    const int INF = 1e9;
+    bool mark[N];
 
-    for (int e = head[v]; e != -1; e = edges[e].next) {
-        int xt = edges[e].v;
-        if (edges[e].cap - edges[e].flow == 0 || mark[xt]) continue;
+    int dfs(int u, int min_cap) {
+        mark[u] = 1;
+        if (u == snk || !min_cap)
+            return min_cap;
         
-        int augment = dfs(xt, min(flow, edges[e].cap - edges[e].flow), snk);
-        if (augment) {
-            edges[e].flow += augment;
-            edges[e ^ 1].flow -= augment;
-            return augment;
+        for (auto &edge : g[u]) {
+            if (!mark[edge.v] && edge.cap > edge.flow) {
+                int augment = dfs(edge.v, min(min_cap, edge.cap - edge.flow));
+
+                if (augment) {
+                    edge.flow += augment;
+                    g[edge.v][edge.opposite].flow -= augment;
+                    return augment;
+                }
+            }
         }
+
+        return 0;
     }
 
-    return 0;
-}
+    int max_flow() {
+        int ret = 0, flow = 0;
+        
+        do {
+            memset(mark, 0, sizeof(mark));
+            flow = dfs(src, INF);
+            ret += flow;
+        } while(flow);
 
-int FordFulkerson(int src, int snk) {
-    int max_flow = 0, flow = 0;
-
-    do {
-        memset(mark, 0, sizeof mark);
-        flow = dfs(src, inf, snk);
-        max_flow += flow;
-    } while(flow);
-
-    return max_flow;
+        return ret;
+    }
 }
 
 int main() {
-    int n, m, src, snk;
     scanf("%d%d%d%d", &n, &m, &src, &snk);
-    fill(head + 1, head + n + 1, -1);
-
     for (int i = 0, u, v, cap; i < m; i++) {
         scanf("%d%d%d", &u, &v, &cap);
-        add_edge(u, v, cap, 0);
+        add_edge(u, v, cap);
     }
 
-    printf("%d\n", FordFulkerson(src, snk));
+    int max_flow = FordFulkerson::max_flow();
+    printf("%d\n", max_flow);
 
     return 0;
 }
