55042733233a62a53e408f984663032a45484d5e
4eyes4u <kosta.grujcic@hotmail.com>
2018-09-16 23:49:08 +0200

    Fix

--- DataStructures/ImplicitTreap.cpp
+++ DataStructures/ImplicitTreap.cpp
@@ -1,167 +1,137 @@
 /*
-  Data structure: Implicit Treap
-  Time complexity: O(logn) per operation (average) [where n is number of elements]
-  Memory complexity: O(n) [where n is number of elements]
+    Name: Implicit treap
+
+    Time complexity: O(logN) per operation
+    Space complexity: O(N)
 
 * * *
-Support every objective function.
-
-In given code maximum on interval and reverse interval are implemented.
+    Merge operation is 'min' + 'swap'.
 */
 
 #include <bits/stdc++.h>
 using namespace std;
 
-class Treap {
-private:
-  struct Node {
+const int INF = 1e9;
+
+struct Node {
     Node *l, *r;
-    int priority, size, value, f;
-    bool reverse;
+    int key, size, pr, val;
+    bool rev;
 
-    Node(int value = 0) {
-      this->l = this->r = nullptr;
-      this->priority = rand();
-      this->size = 1;
-      this->value = value;
-      this->f = value;
-      this->reverse = 0;
+    Node(int key, int pr) {
+        this->key = key;
+        this->pr = pr;
+        this->size = 1;
+        this->rev = 0;
+        this->val = key;
+        this->l = nullptr;
+        this->r = nullptr;
     }
-  } * root;
+} *root;
 
-  int size(Node *v) { return v ? v->size : 0; }
-  
-  void propagate(Node *v) {
-    if (v && v->reverse) {
-      v->reverse = 0;
-      swap(v->l, v->r);
+int get_size(Node *v) {
+    return v ? v->size : 0;
+}
 
-      if (v->l)
-        v->l->reverse ^= 1;
-      if (v->r)
-        v->r->reverse ^= 1;
+int get_val(Node *v) {
+    return v ? v->val : INF;
+}
+
+void update(Node *v) {
+    if (v) {
+        v->size = 1 + get_size(v->l) + get_size(v->r);
+        v->val = min(v->key, min(get_val(v->l), get_val(v->r)));
     }
-  }
-  
-  int f(Node *v) {
-    propagate(v);
-    return v ? v->f : 0;
-  }
+}
 
-  void update_size(Node *v) {
-    if (v)
-      v->size = size(v->l) + size(v->r) + 1;
-  }
-
-  void update_f(Node *v) {
-    if (v)
-      v->f = max(max(f(v->l), f(v->r)), v->value);
-  }
-
-  void split(Node *v, Node *&l, Node *&r, int key, int lager = 0) {
-    if (!v)
-      return void(l = r = nullptr);
-    propagate(v);
-    int implicit_key = lager + size(v->l);
-    if (key > implicit_key) {
-      split(v->r, v->r, r, key, implicit_key + 1);
-      l = v;
-    } else {
-      split(v->l, l, v->l, key, lager);
-      r = v;
+void propagate(Node *v) {
+    if (v && v->rev) {
+        swap(v->l, v->r);
+        if (v->l)
+            v->rev ^= 1;
+        if (v->r)
+            v->rev ^= 1;
+        v->rev = 0;
     }
+}
 
-    update_size(v);
-    update_f(v);
-  }
-
-  void merge(Node *&root, Node *l, Node *r) {
+void merge(Node *&v, Node *l, Node *r) {
     propagate(l);
     propagate(r);
-    if (!l || !r)
-      root = l ? l : r;
-    else if (l->priority < r->priority) {
-      merge(l->r, l->r, r);
-      root = l;
-    } else {
-      merge(r->l, l, r->l);
-      root = r;
+
+    if (!l)
+        v = r;
+    else if (!r)
+        v = l;
+    else if (l->pr > r->pr) {
+        merge(l->r, l->r, r);
+        v = l;
+    }
+    else {
+        merge(r->l, l, r->l);
+        v = r;
     }
 
-    update_size(root);
-    update_f(root);
-  }
+    update(v);
+}
 
-  void delete_treap(Node *v) {
-    if (!v)
-      return;
-    delete_treap(v->l);
-    delete_treap(v->r);
-    delete (v);
-  }
+void split(Node *&v, Node *&l, Node *&r, int key, int add = 0) {
+    propagate(v);
+    if (!v) {
+        l = r = nullptr;
+        return;
+    }
 
-  void insert(Node *&root, Node *v, int pos) {
-    Node *t1, *t2;
-    split(root, t1, t2, pos);
-    merge(t1, t1, v);
-    merge(root, t1, t2);
-  }
+    int implicit_key = 1 + add + get_size(v->l);
+    if (key <= implicit_key) {
+        split(v->l, l, v->l, key, add);
+        r = v;
+    }
+    else {
+        split(v->r, v->r, r, key, 1 + add + get_size(v->r));
+        l = v;
+    }
 
-  void reverse(Node *root, int l, int r) {
-    Node *t1, *t2, *t3;
-    split(root, t1, t2, l);
-    split(t2, t2, t3, r - l + 1);
-    t2->reverse ^= 1;
-    merge(root, t1, t2);
-    merge(root, root, t3);
-  }
+    propagate(v);
+}
 
-  int query(Node *root, int l, int r) {
-    Node *t1, *t2, *t3;
-    split(root, t1, t2, l);
-    split(t2, t2, t3, r - l + 1);
-    int ret = t2->f;
-    merge(root, t1, t2);
-    merge(root, root, t3);
+void reverse(Node *v, int l, int r) {
+    Node *u1, *u2;
+    split(v, v, u1, l, 0);
+    split(u1, u1, u2, r - l + 1, 0);
+    u1->rev ^= 1;
+    merge(v, v, u1);
+    merge(v, v, u2);
+}
+
+int query(Node *v, int l, int r) {
+    Node *u1, *u2;
+    split(v, v, u1, l, 0);
+    split(u1, u1, u2, r - l + 1, 0);
+    int ret = get_val(u1);
+    merge(v, v, u1);
+    merge(v, v, u2);
+    
     return ret;
-  }
-
-  void print_treap(Node *v) {
-    if (!v)
-      return;
-    print_treap(v->l);
-    printf("%d\n", v->value);
-    print_treap(v->r);
-  }
-
-public:
-  Treap(int root_value) { root = new Node(root_value); }
-
-  ~Treap() { delete_treap(this->root); }
-
-  void insert(int value, int pos) { insert(this->root, new Node(value), pos); }
-
-  void reverse(int l, int r) { reverse(this->root, l, r); }
-
-  int query(int l, int r) { return query(this->root, l, r); }
-
-  void print_treap() { print_treap(this->root); }
-};
+}
 
 int main() {
-  Treap *t = new Treap(5);
-  t->insert(7, 1);
-  t->insert(10, 2);
-  t->insert(3, 3);
-  t->insert(17, 4);
+    int n, q;
+    scanf("%d%d", &n, &q);
 
-  printf("%d\n", t->query(0, 1));
-  t->reverse(1, 3);
-  printf("%d\n", t->query(0, 4));
-  printf("%d\n", t->query(2, 3));
-  printf("%d\n", t->query(0, 1));
+    for (int i = 0, key; i < n; i++) {
+        scanf("%d", &key);
+        Node *to_add = new Node(key, rand());
+        merge(root, root, to_add);
+    }
 
-  delete (t);
+    for (int i = 0, type, l, r; i < q; i++) {
+        scanf("%d%d%d", &type, &l, &r);
+        if (type == 0)
+            reverse(root, l, r);
+        else
+            printf("%d\n", query(root, l, r));
+    }
 
-  return 0;
+    return 0;
 }
