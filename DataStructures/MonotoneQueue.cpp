9a6f2df7334efdb029211d58b0ff2106db71b405
4eyes4u <kosta.grujcic@hotmail.com>
2018-09-23 17:27:15 +0200

    Monotone queue fix

--- DataStructures/MonotoneQueue.cpp
+++ DataStructures/MonotoneQueue.cpp
@@ -1,28 +1,31 @@
 /*
-  Data structure: Monotone queue
-  Time complexity: O(1) amortized per operation
-  Memory complexity: O(n) [where n is number of elements in queue]
+    Name: Monotone queue
+
+    Time complexity: O(1) amortized per operation
+    Space complexity: O(N)
 
 * * *
-RMQ operation is minimum.
+    RMQ operation is minimum.
 */
 
 #include <bits/stdc++.h>
 using namespace std;
 
-void add (deque<int> &dq, int x)
-{
-  while (!dq.empty() && dq.back()>x) dq.pop_back();
+dequeue<int> dq;
+
+void add(int x) {
+    while (!dq.empty() && dq.back() > x)
+        dq.pop_back();
+  
   dq.push_back(x);
 }
 
-int get_min (deque<int> &dq)
-{
-  if (!dq.empty()) return dq.front();
-  return -1;
+int get_min() {
+    if (!dq.empty())
+        return dq.front();
+    return -1;
 }
 
-int main()
-{
-  return 0;
+int main() {
+    return 0;
 }
