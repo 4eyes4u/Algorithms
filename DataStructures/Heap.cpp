5675204ab3acd8e5b312caa8a310aed5eec8c724
4eyes4u <kosta.grujcic@hotmail.com>
2018-09-23 17:21:10 +0200

    Fixed heap

--- DataStructures/Heap.cpp
+++ DataStructures/Heap.cpp
@@ -1,90 +1,141 @@
 /*
-  Data structure: D-ary heap
-  Time complexity: O(logn) per operaton [where n is number of nodes]
-  Memory complexity: O(n)
+    Name: D-ary heap
+
+    Time complexity: O(logN) per operation
+    Space complexity: O(N)
 */
 
-#include <bits/stdc++.h>
-using namespace std;
+#include <vector>
+#include <iostream>
+#include <stdexcept>
+#include <utility>
 
-const int inf = 1e9;
+using std::cin;
+using std::cout;
+using std::endl;
 
-class dHeap {
-private:
-    vector<int> keys;
-    int d, n;
-    
-    int parent(int x) { return (x - 1) / d; }
-    
-    int child(int x, int i) { return d * x + i; }
-    
-    void fix_down(int x) {
-        for (int i = 1; i <= d; i++) {
-            if (x * d + i > n) break;
-            
-            if (keys[child(x, i)] < keys[x]) {
-                swap(keys[child(x, i)], keys[x]);
-                fix_down(child(x, i));
-                return;
-            }
-        }
-    }
-    
-    void fix_up(int x) {
-        if (keys[x] < keys[parent(x)]) {
-            swap(keys[x], keys[parent(x)]);
-            fix_up(parent(x));
-        }
-    }
-    
+template <typename T>
+class Heap {
+    std::vector<T> keys;
+    int degree;
+
 public:
-	bool is_empty() { return n == 0; }
+    Heap(int degree) {
+        this->keys = std::vector<T>();
+        this->degree = degree;
+    }
+
+    Heap(std::vector<T> &keys, int degree) {
+        this->keys = std::vector<T>(keys);
+        this->degree = degree;
+        for (int i = ((int)keys.size() - 2) / degree; i >= 0; i--)
+            fix_down(i);
+    }
+
+    bool is_empty() {
+        return keys.size() == 0;
+    }
 
     void clear() {
         keys.clear();
-        n = 0;
     }
-    
-    void init(int *a, int sz) {
-        n = sz;
-        keys.resize(n);
-        for (int i = 0; i < n; i++) keys[i] = a[i];
-        for (int i = (n - 2) / d; i >= 0; i--) fix_down(i);
-    }
-    
-    int find_min() {
-        if (is_empty()) return inf;
-        return keys[0];
-    }
-    
-    void insert_node(int val) {
-        keys.push_back(val);
-        n++;
-        fix_up(n);
-    }
-    
-    void delete_min() {
-        if (is_empty()) return;
+
+    T *find_min() {
+        if (is_empty())
+            return nullptr;
         
-        n--;
-        swap(keys[0], keys[n]);
-        keys.pop_back();
-        fix_down(0);
+        return &keys[0];
     }
-    
-    void decrease_key(int x, int val) {
-        if (x >= n) return;
+
+    T *delete_min() {
+        if (is_empty())
+            return nullptr;
+
+        T *ret = &keys[0];
+        std::swap(keys[0], keys.back());
+        keys.pop_back();
+        
+        if (!is_empty())
+            // there is nothing to fix
+            fix_down(0);
+        return ret;
+    }
+
+    void insert(T val) {
+        keys.emplace_back(val);
+        fix_up((int)keys.size() - 1);
+    }
+
+    bool decrease_key(int x, T val) {
+        if (x >= (int)keys.size() || keys[x] <= val)
+            return false;
+        
         keys[x] = val;
         fix_up(x);
+        return true;
     }
-    
-    void delete_node(int x) {
-        if (is_empty()) return;
-        decrease_key(x, -inf);
-        delete_min();
+
+private:
+    int parent_index(int x) {
+        return (x - 1) / degree;
+    }
+
+    int child_index(int x, int i) {
+        return x * degree + i;
+    }
+
+    void fix_down(int x) {
+        if (x >= (int)keys.size())
+            throw std::runtime_error("Index is too large.");
+
+        for (int i = 1; i <= degree; i++) {
+            if (x * degree + i <= (int)keys.size()) {
+                int child = child_index(x, i);
+                if (keys[child] < keys[x]) {
+                    std::swap(keys[child], keys[x]);
+                    
+                    if (child < (int)keys.size())
+                        // checking if 'child' is a leaf
+                        fix_down(child);
+                    return;
+                }
+            }
+        }
+    }
+
+    void fix_up(int x) {
+        if (x >= (int)keys.size())
+            throw std::runtime_error("Index is too large.");
+        if (!x)
+            // root
+            return;
+
+        int parent = parent_index(x);
+        if (keys[x] < keys[parent]) {
+            std::swap(keys[x], keys[parent]);
+
+            if (parent)
+                // checking if 'parent' is the root
+                fix_up(parent);
+        }
     }
 };
 
 int main() {
+    Heap<int> *heap = new Heap<int>(2);
+    heap->insert(5);
+    heap->insert(2);
+    heap->insert(4);
+    heap->insert(3);
+    heap->insert(6);
+
+    int *min_key = heap->find_min();
+    cout << *min_key << endl;
+
+    heap->delete_min();
+    
+    min_key = heap->find_min();
+    cout << *min_key << endl;
+
     return 0;
 }
