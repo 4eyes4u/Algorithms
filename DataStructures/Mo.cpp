5a5356a8aa40d99f86a4afcde240320739229443
4eyes4u <kosta.grujcic@hotmail.com>
2018-09-25 00:14:12 +0200

    Fixed Mo

--- DataStructures/Mo.cpp
+++ DataStructures/Mo.cpp
@@ -1,67 +1,76 @@
 /*
-  Data structure: Mo's algorithm
-  Time complexity: O(sqrt(n))*X per operation [where n is number of elements and X is complexity of bucket update/query]
-  Memory complexity: O(n) [were n is number of elements]
+    Name: Mo's algorithm
+    
+    Time complexity: O((Q + N) * qrt(N)) for offline processing
+    Space complexity: O(N)
 
 * * *
-Calculating sum on interval.
+    Calculating sum on interval.
 */
 
 #include <bits/stdc++.h>
 using namespace std;
 
-const int N=1e5+10;
-const int Q=1e4+10;
-const int BLOCKS=320;
+const int N = 1e5 + 10;
+const int Q = 1e5 + 10;
+const int BLOCK_SIZE = 320;
+const int BLOCKS = ceil(N / BLOCK_SIZE);
 
-struct Query
-{
-  int l, r, idx;
-  Query(): l(), r(), idx() {}
-  Query(int _l, int _r, int _idx): l(_l), r(_r), idx(_idx) {}
-  bool operator < (const Query &q) const
-  {
-    return r<q.r;
-  }
+struct Query {
+    int l, r, idx;
+
+    Query(int l, int r, int idx) {
+        this->l = l;
+        this->r = r;
+        this->idx = idx;
+    }
+
+    bool operator < (const Query &other) const {
+        return r < other.r;
+    }
 };
 
-vector<Query> mo[BLOCKS+10];
-int n, q, a[N];
+vector<Query> mo[BLOCKS];
+int a[N];
 long long sol[Q];
 
-int main()
-{
-  scanf("%d%d", &n, &q);
-  for (int i=1;i<=n;i++) scanf("%d", &a[i]);
-  for (int i=0;i<q;i++)
-  {
-    int l, r;
-    scanf("%d%d", &l, &r);
-    mo[l/BLOCKS].push_back(*new Query(l, r, i));
-  }
+int main() {
+    int n;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+        scanf("%d", a + i);
 
-  for (int i=0;i<BLOCKS;i++) sort(mo[i].begin(), mo[i].end());
-
-  for (int i=0;i<BLOCKS;i++)
-  {
-    if (mo[i].empty()) continue;
-
-    int L=mo[i][0].l, R=L-1;
-    long long sum=0;
-
-    for (auto qxt: mo[i])
-    {
-      int l=qxt.l, r=qxt.r, idx=qxt.idx;
-
-      while (R<r) sum+=1ll*a[++R];
-      while (L<l) sum-=1ll*a[L++];
-      while (L>l) sum+=1ll*a[--L];
-
-      sol[idx]=sum;
+    int q;
+    scanf("%d", &q);
+    for (int i = 0, l, r; i < q; i++) {
+        scanf("%d%d", &l, &r);
+        mo[l / BLOCKS].emplace_back(l, r, i);
     }
-  }
 
-  for (int i=0;i<q;i++) printf("%lld\n", sol[i]);
+    for (int i = 0; i < BLOCKS; i++)
+        sort(mo[i].begin(), mo[i].end());
+    
+    for (int i = 0; i < BLOCKS; i++) {
+        if (mo[i].size()) {
+            int l = mo[i][0].l, r = l - 1;
+            long long sum = 0;
 
-  return 0;
+            for (auto query : mo[i]) {
+                while (r < query.r)
+                    sum += a[++r];
+                while (l < query.l)
+                    sum -= a[l++];
+                while (l > query.l)
+                    sum += a[--l];
+                
+                sol[query.idx] = sum;
+            }
+        }
+    }
+
+    for (int i = 0; i < q; i++)
+        printf("%lld ", sol[i]);
+    puts("");
+
+    return 0;
 }
