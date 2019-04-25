da129b87118d8fc6cd6e0453fb0773f3236b1170
Kosta Grujčić <kosta.grujcic@hotmail.com>
2019-04-25 09:02:38 +0200

    Added Manacher's algorithm

---
+++ Strings/Manacher.cpp
@@ -0,0 +1,54 @@
+/*
+    Name: Manacher's algorithm
+
+    Time complexity: O(N)
+    Space complexity: O(N)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// char not present in the input
+const char DUMMY = '#';
+
+pair<int, int> Manacher(const string &s, string &pal) {
+    int n = s.length();
+    string aux(2*n + 1, DUMMY);
+    for (int i = 0; i < n; i++)
+        aux[2*i + 1] = s[i];
+
+    vector<int> dp(2*n + 1, 0);
+    int center = 0, r = 0;
+    for (int i = 1; i < 2*n + 1; i++) {
+        int mirror_i = 2*center - i;
+        if (r > i)
+            dp[i] = min(r - i, dp[mirror_i]);
+        while (aux[i + 1 + dp[i]] == aux[i - 1 - dp[i]])
+            dp[i]++;
+        if (i + dp[i] > r) {
+            center = i;
+            r = i + dp[i];
+        }
+    }
+
+    int pal_len = 0, pal_center = 0;
+    for (int i = 0; i < 2*n + 1; i++) {
+        if (dp[i] > pal_len) {
+            pal_len = dp[i];
+            pal_center = i;
+        }
+    }
+    pal = s.substr((pal_center - pal_len) / 2, pal_len);
+
+    return {pal_len, pal_center};
+}
+
+int main() {
+    string s;
+    cin >> s;
+    string pal;
+    auto ret = Manacher(s, pal);
+    cout << pal << endl;
+
+    return 0;
+}
