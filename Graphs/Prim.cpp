936fd61cc0c34ea9bc023ba5b2e812dca24ed66f
Kosta Grujčić <kosta.grujcic@hotmail.com>
2019-04-27 17:02:06 +0200

    Updated Prim's algorithm

--- Graphs/Prim.cpp
+++ Graphs/Prim.cpp
@@ -1,6 +1,8 @@
 /*
-    Algorithm: Prim's algorithm
-    Complexity: O(m*logn) [where n is number of nodes and m number of edges in the graph]
+    Name: Prim's algorithm
+
+    Time complexity: O(M * logN)
+    Space complexity: O(N + M)
 */
 
 #include <bits/stdc++.h>
@@ -14,20 +16,17 @@ bool added[N];
 long long Prim (int source = 1) {
     long long mst = 0;
     priority_queue<pair<int, int>,
-                    vector<pair<int, int>>,
-                    greater<pair<int, int>>> pq;
-    
+                   vector<pair<int, int>>,
+                   greater<pair<int, int>>> pq;
     for (auto pxt : g[source])
         pq.emplace(pxt);
 
     while (!pq.empty()) {
         int w = pq.top().first;
         int v = pq.top().second;
-        
         if (!added[v]) {
             added[v] = 1;
             mst += 1ll * w;
-
             for (auto pxt : g[v])
                 if (!added[pxt.second])
                     pq.emplace(pxt);
@@ -39,9 +38,10 @@ long long Prim (int source = 1) {
 
 int main() {
     int n, m;
-    scanf ("%d%d", &n, &m);
+    scanf("%d%d", &n, &m);
     for (int i = 0, u, v, w; i < m; i++) {
-        scanf ("%d%d%d", &u, &v, &w);
+        scanf("%d%d%d", &u, &v, &w);
+        // edges are added as (weight, node)
         g[u].push_back({w, v});
         g[v].push_back({w, u});
     }
