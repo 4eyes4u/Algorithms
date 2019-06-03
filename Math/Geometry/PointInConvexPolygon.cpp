35c60dd6004df702cef8975d16b3cf38ae6b6483
Kosta Grujčić <kosta.grujcic@hotmail.com>
2019-06-03 22:29:44 +0200

    Minor fixes

--- Math/Geometry/PointInConvexPolygon.cpp
+++ Math/Geometry/PointInConvexPolygon.cpp
@@ -20,12 +20,14 @@ struct Point {
 } pts[N];
 
 long long ccw(Point O, Point A, Point B) {
-    return 1ll * (A.x - O.x) * (B.y - O.y) - (B.x - O.x) * (A.y - O.y);
+    return 1ll * ((A.x - O.x)*(B.y - O.y) - (B.x - O.x)*(A.y - O.y));
 }
 
 bool in_triangle(Point A, Point B, Point C, Point P) {
     bool left = ccw(A, B, P) >= 0 && ccw(B, C, P) >= 0 && ccw(C, A, P) >= 0;
-    bool right = ccw(A, B, P) <= 0 && ccw(B, C, P) <= 0 && ccw(C, A, P) <= 0; 
+    bool right = ccw(A, B, P) <= 0 && ccw(B, C, P) <= 0 && ccw(C, A, P) <= 0;
+
+    return left || right;
 }
 
 bool check(int n, Point P) {
@@ -39,8 +41,7 @@ bool check(int n, Point P) {
             l = mid + 1;
             last = mid;
         }
-        else
-            r = mid - 1;
+        else r = mid - 1;
     }
 
     return last != -1 && in_triangle(pts[0], pts[last], pts[last + 1], P);
